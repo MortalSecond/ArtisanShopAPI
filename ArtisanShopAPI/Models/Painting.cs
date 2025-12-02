4d8813bedc8daaf050ab6946b9133e90797e4078
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-01 23:10:08 -0600

    Add EFC model for the paintings.

---
+++ ArtisanShopAPI/Models/Painting.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArtisanShopAPI.Models
+{
+    public class Painting
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        [Column("title")]
+        public string? Title { get; set; }
+
+        [MaxLength(50)]
+        [Column("details")]
+        public string? Details { get; set; }
+
+        [MaxLength(200)]
+        [Column("image_url")]
+        public string? ImageURL { get; set; }
+
+        [Column("height")]
+        public int? Height { get; set; }
+
+        [Column("width")]
+        public int? Width { get; set; }
+    }
+}

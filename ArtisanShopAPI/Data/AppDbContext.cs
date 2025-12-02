455ce5298d944d22502a75ab9de931d7a16fd804
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-01 23:19:10 -0600

    Add AppDbContext and Data folder for EFC.

---
+++ ArtisanShopAPI/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ArtisanShopAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtisanShopAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Painting> Paintings { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Painting>().ToTable("paintings");
+        }
+    }
+}

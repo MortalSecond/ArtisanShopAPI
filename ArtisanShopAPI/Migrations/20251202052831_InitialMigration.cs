80f8e83e2418fe21538d808f17728665f1208ce5
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-01 23:31:12 -0600

    Add initial EFC migration to database.

---
+++ ArtisanShopAPI/Migrations/20251202052831_InitialMigration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ArtisanShopAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "paintings",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    details = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    image_url = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    height = table.Column<int>(type: "integer", nullable: true),
+                    width = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_paintings", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "paintings");
+        }
+    }
+}

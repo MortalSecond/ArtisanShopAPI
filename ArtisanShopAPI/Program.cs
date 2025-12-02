4c3044f18e8d42138b3104244fbfff86869b1622
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-01 23:28:24 -0600

    Wire up the program.cs to EFC, PostgreSQL, and Angular.

--- ArtisanShopAPI/Program.cs
+++ ArtisanShopAPI/Program.cs
@@ -1,10 +1,25 @@
+using ArtisanShopAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add Database Context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+
+// Add Angular to CORS rules
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -15,9 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
+app.UseCors("AllowAngular");
 app.MapControllers();
-
 app.Run();

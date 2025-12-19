ff5cafbb8edeed607ad292fe4ebf9568d2ac2360
MortalSecond <irondiego_jkl@hotmail.com>
2025-12-19 15:00:37 -0600

    Nevermind. Revert all Railpack files; i'll use Docker instead.

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Running database migrations..."
-dotnet ef database update --no-build
-
-echo "Starting application..."
-dotnet ArtisanShopAPI.dll
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Starting ASP.NET Core application..."
-
-export ASPNETCORE_URLS=http://0.0.0.0:${PORT}
-
-dotnet ArtisanShopAPI.dll
